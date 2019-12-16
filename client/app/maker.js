2e638ec0e1b31e4831dd75ef40a41f16d695ce09
Christian <cmr6117@gmail.com>
2019-12-16 07:42:35 -0500

    Ads v4

--- client/app/maker.js
+++ client/app/maker.js
@@ -188,7 +188,7 @@ const PauseButton = function(props) {
 
 const Ad = function(props) {
     return (
-        <img src="/img/bannerAd.jpg" alt="Banner Ad Placeholder"/>
+        <img src="../assets/img/bannerAd.jpg" alt="Banner Ad Placeholder"/>
     );        
 };
 
